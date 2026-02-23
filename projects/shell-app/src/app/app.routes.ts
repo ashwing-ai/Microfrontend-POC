73f9e565d157c3a384e158c63a6bbd535c59f29f
ashwing-ai <ashwin.gadve@coditas.com>
2026-02-23 17:45:23 +0530

    added routes

--- projects/shell-app/src/app/app.routes.ts
+++ projects/shell-app/src/app/app.routes.ts
@@ -5,12 +5,12 @@ export const routes: Routes = [
   {
     path: 'mfe1',
     loadChildren: () =>
-      loadRemoteModule('mfe1', './routes').then((m) => m.MFE1_ROUTES),
+      loadRemoteModule('mfe1', './routes').then((m) => m.routes),
   },
   {
     path: 'mfe2',
     loadChildren: () =>
-      loadRemoteModule('mfe2', './routes').then((m) => m.MFE2_ROUTES),
+      loadRemoteModule('mfe2', './routes').then((m) => m.routes),
   },
   { path: '', redirectTo: 'mfe1', pathMatch: 'full' },
 ];
